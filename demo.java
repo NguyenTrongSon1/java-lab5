fcfbd5039deec290e24f59be3deaf6571b080003
NguyenTrongSon1 <65001546+NguyenTrongSon1@users.noreply.github.com>
2020-11-05 21:55:50 +0700

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,39 @@
+package mysql;
+
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class demo {
+    public static void main(String[] args) {
+        try(
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/ebookshop", "root", ""
+                );
+                Statement stmt = conn.createStatement();
+        ){
+            ResultSet rset = stmt.executeQuery("select * from books");
+            ResultSetMetaData resetMD = rset.getMetaData();
+
+            int numColums = resetMD.getColumnCount();
+            for (int i = 1;i<= numColums;++i){
+                System.out.printf("%-30s",resetMD.getColumnName(i));
+            }
+            System.out.println();
+
+            for (int i = 1;i<=numColums;++i){
+
+                System.out.printf("%-30s","(" + resetMD.getColumnClassName(i) + ")");
+            }
+            System.out.println();
+
+            while (rset.next()){
+                for (int i =1;i <= numColums;++i){
+                    System.out.printf("%-30s",rset.getString(i));
+                }
+                System.out.println();
+            }
+        }catch (SQLException ex){
+            ex.printStackTrace();
+        }
+    }
+}
